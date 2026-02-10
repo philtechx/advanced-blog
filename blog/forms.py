bb4da15825a443136d6bf4cdfd7d8dcdc0ae0840
Phil-py <filimonisamsoni2@gmail.com>
2026-02-10 23:16:46 +0300

    Resolve merge conflict

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
@@ -35,41 +35,4 @@ class LoginForm(AuthenticationForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={
         'class': 'form-control', 'placeholder': 'Password'
     }))
-=======
-from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-# ====================
-# Registration Form
-# ====================
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
-        'class': 'form-control', 'placeholder': 'Email'
-    }))
-    username = forms.CharField(required=True, widget=forms.TextInput(attrs={
-        'class': 'form-control', 'placeholder': 'Username'
-    }))
-    password1 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={
-        'class': 'form-control', 'placeholder': 'Password'
-    }))
-    password2 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={
-        'class': 'form-control', 'placeholder': 'Confirm Password'
-    }))
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
-
-
-# ====================
-# Login Form
-# ====================
-class LoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control', 'placeholder': 'Username'
-    }))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'form-control', 'placeholder': 'Password'
-    }))
->>>>>>> 4a481a0 (Updated blog added comment system and authentication)

7978c8eec749e6d6d8a0658b5a2f304ebb4c900e
Phil-py <filimonisamsoni2@gmail.com>
2026-02-10 23:35:19 +0300

    Resolve merge conflict

--- blog/apps.py
+++ blog/apps.py
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 from django.apps import AppConfig
-
 
 class BlogConfig(AppConfig):
     name = 'blog'
-=======
-from django.apps import AppConfig
-
-
-class BlogConfig(AppConfig):
-    name = 'blog'
->>>>>>> 4a481a0 (Updated blog added comment system and authentication)

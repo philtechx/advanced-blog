3cc75ddc6a68d7855a827aef9c0d9321fd0c92d3
Phil-py <filimonisamsoni2@gmail.com>
2026-02-11 03:21:39 +0300

    Postgres database setting

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from .models import Category
 
 def categories_processor(request):
@@ -6,12 +5,3 @@ def categories_processor(request):
     Returns all categories to templates (e.g., sidebar).
     """
     return {'categories': Category.objects.all()}
-=======
-from .models import Category
-
-def categories_processor(request):
-    """
-    Returns all categories to templates (e.g., sidebar).
-    """
-    return {'categories': Category.objects.all()}
->>>>>>> 4a481a0 (Updated blog added comment system and authentication)
